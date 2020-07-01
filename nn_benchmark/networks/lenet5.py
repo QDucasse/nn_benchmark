d82378b425003a90db7c4f4ff765b575976dcae5
QDucasse <quentin.ducasse@ensta-bretagne.org>
2020-07-01 20:27:46 +0200

    README updated
    Former-commit-id: 7485ab408c2589945d0078d92c9d4f0f46dad60d

--- nn_benchmark/networks/lenet5.py
+++ nn_benchmark/networks/lenet5.py
@@ -19,13 +19,16 @@ class LeNet5(nn.Module):
         super(LeNet5, self).__init__()
 
         self.feature_extractor = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=6, kernel_size=5, stride=1),
+            nn.Conv2d(in_channels=in_channels, out_channels=6,
+                      kernel_size=5, stride=1),
             nn.Tanh(),
             nn.AvgPool2d(kernel_size=2),
-            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1),
+            nn.Conv2d(in_channels=6, out_channels=16,
+                      kernel_size=5, stride=1),
             nn.Tanh(),
             nn.AvgPool2d(kernel_size=2),
-            nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5, stride=1),
+            nn.Conv2d(in_channels=16, out_channels=120,
+                      kernel_size=5, stride=1),
             nn.Tanh()
         )
 
